ecfd3e5256237d48f51dceefd1a974a9062dfe0d
Martin Ueding <dev@martin-ueding.de>
2011-01-09 22:21:34 +0100

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="">
+	<head>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>Urban Terror</title>
+	</head>
+	<body>
+	
+	<a href="config.php">Waffen Binding Generator</a>
+	<br /><br />
+	<b>Log Tool</b>
+	<br />
+	<a href="LogToolR4.jar">Download LogToolR4.jar</a>
+	<br />
+	<a href="urt-start.command">Download Startskript für UrT</a>
+
+	</body>
+</html>
